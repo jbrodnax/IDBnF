50a4a1292d94d3c0664dd69db578af00938e5086
jbrodnax <jbrodnax55@gmail.com>
2017-12-31 00:34:27 +0000

    added jump opcode defs to disas

--- disassembler_modules/disas.h
+++ disassembler_modules/disas.h
@@ -1,3 +1,16 @@
 #include "../calltrace.h"
 #include <inttypes.h>
 #include <capstone/capstone.h>
+
+#define JB	0x72
+#define JNB	0x73
+#define JE	0x74
+#define JNE	0x75
+#define JBE	0x76
+#define JNBE	0x77
+#define JS	0x78
+#define JNS	0x79
+#define JP	0x7A
+#define JNP	0x7B
+#define JL	0x7C
+#define JNL	0x7A
